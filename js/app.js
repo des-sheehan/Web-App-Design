be41d10d4faa41f07ac9d768bce1ff84d85d6059
des-sheehan <77647491+des-sheehan@users.noreply.github.com>
2021-05-12 15:50:00 +0100

    version 3
    charts added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+
+// ALERT BANNER ------------------------------------------------------
+
 const alertBanner = document.getElementById("alert");
 
 alertBanner.innerHTML = 
@@ -13,4 +16,115 @@ alertBanner.addEventListener('click', e => {
     if (element.classList.contains("alert-banner-close")) {
         alertBanner.style.display = "none"
     }
+});
+
+
+// LINE GRAPH ------------------------------------------------------
+
+const trafficCanvas = document.getElementById("traffic-chart")
+
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31" ],
+    datasets: [{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+        backgroundColor: 'rgba(116, 119, 191, 0.3)',
+        fill: true,
+        borderWidth: 1,
+        tension: 0.3,
+    }]
+}
+
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duaration: 0
+    },
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
+
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+});
+
+
+// BAR CHART ------------------------------------------------------
+
+const dailyCanvas = document.getElementById("daily-chart");
+
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        labels: '# of Hits',
+        data: [75, 155, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1
+    }]
+};
+
+const dailyOptions = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+             display: false
+        }
+       
+    }
+};
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
+
+
+// DONUT CHART ------------------------------------------------------
+
+const mobileCanvas = document.getElementById("donut-chart");
+
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phone"],
+    datasets: [{
+        labels: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6C8',
+        ]   
+    }]
+};
+
+const mobileOptions = {
+    plugins: {
+        legend: {
+            position: 'right',
+            labels: {
+                boxWidth: 20,
+                fontStyle: 'bold',
+            }
+        }
+    }
+};
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
 });
--- js/charts.js
+++
@@ -1,7 +0,0 @@
-
-const trafficCanvas = document.getElementById("traffic-chart")
-
-let trafficData = {
-    labels: ["S", "M", "T", "W", "T", "F", "S"],
-    datasets: 
-}
